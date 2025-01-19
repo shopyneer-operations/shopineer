b3d702cea71b4a6de0b8a631b938722e24c14b92
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-01-19 18:06:09 +0200

    Small change

--- storefront/src/middleware.ts
+++ storefront/src/middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 const PUBLISHABLE_API_KEY = process.env.NEXT_PUBLIC_MEDUSA_PUBLISHABLE_KEY
-const DEFAULT_REGION = process.env.NEXT_PUBLIC_DEFAULT_REGION || "us"
+const DEFAULT_REGION = process.env.NEXT_PUBLIC_DEFAULT_REGION || "eg"
 
 const regionMapCache = {
   regionMap: new Map<string, HttpTypes.StoreRegion>(),
@@ -28,6 +28,8 @@ async function getRegionMap() {
         tags: ["regions"],
       },
     }).then((res) => res.json())
+
+    console.log("🤯", regions)
 
     if (!regions?.length) {
       notFound()
@@ -142,5 +144,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|favicon.ico|.*\\.png|.*\\.jpg|.*\\.gif|.*\\.svg).*)"], // prevents redirecting on static files
+  matcher: [
+    "/((?!api|_next/static|favicon.ico|.*\\.png|.*\\.jpg|.*\\.gif|.*\\.svg).*)",
+  ], // prevents redirecting on static files
 }
