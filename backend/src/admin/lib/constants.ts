392784767a0ada0f7f2f907727d9e011d1093104
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-01-20 14:42:35 +0200

    Removed all admin customizations

--- backend/src/admin/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-const constants = {
-  SUPPLIERS_LIMIT: 15,
-};
-
-export default constants;
--- backend/src/admin/lib/sdk.ts
+++
@@ -1,9 +0,0 @@
-import Medusa from "@medusajs/js-sdk";
-
-export const sdk = new Medusa({
-  baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:9000" : "backend-production-b092.up.railway.app",
-  debug: process.env.NODE_ENV === "development",
-  auth: {
-    type: "session",
-  },
-});
