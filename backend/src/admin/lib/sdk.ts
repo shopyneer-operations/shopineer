5d7a27f9f5b978015400ba9def0772eb7921eca1
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-01-31 15:17:36 +0200

    FIX: url

--- backend/src/admin/lib/sdk.ts
+++ backend/src/admin/lib/sdk.ts
@@ -1,7 +1,10 @@
 import Medusa from "@medusajs/js-sdk";
 
 export const sdk = new Medusa({
-  baseUrl: process.env.NODE_ENV === "development" ? "http://localhost:9000" : "backend-production-b092.up.railway.app",
+  baseUrl:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:9000"
+      : "https://backend-production-b092.up.railway.app/",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
