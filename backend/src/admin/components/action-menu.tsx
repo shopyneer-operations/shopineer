2b850a0d8dbcb2be695d37437219bb18ae74fd57
moaazbhnas228 <moaaz.bhnas@onecommerce.group>
2025-02-03 20:10:09 +0200

    Small fix

--- backend/src/admin/components/action-menu.tsx
+++ backend/src/admin/components/action-menu.tsx
@@ -1,21 +1,19 @@
 import { DropdownMenu, IconButton, clx } from "@medusajs/ui";
 import { EllipsisHorizontal } from "@medusajs/icons";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export type Action = {
   icon: React.ReactNode;
   label: string;
   disabled?: boolean;
-} & (
-  | {
-      to: string;
-      onClick?: never;
-    }
-  | {
-      onClick: () => void;
-      to?: never;
-    }
-);
+} & //   | {
+//       to: string;
+//       onClick?: never;
+//     }
+{
+  onClick: () => void;
+  to?: never;
+};
 
 export type ActionGroup = {
   actions: Action[];
@@ -44,41 +42,41 @@ export const ActionMenu = ({ groups }: ActionMenuProps) => {
           return (
             <DropdownMenu.Group key={index}>
               {group.actions.map((action, index) => {
-                if (action.onClick) {
-                  return (
-                    <DropdownMenu.Item
-                      disabled={action.disabled}
-                      key={index}
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        action.onClick();
-                      }}
-                      className={clx("[&_svg]:text-ui-fg-subtle flex items-center gap-x-2", {
-                        "[&_svg]:text-ui-fg-disabled": action.disabled,
-                      })}
-                    >
-                      {action.icon}
-                      <span>{action.label}</span>
-                    </DropdownMenu.Item>
-                  );
-                }
-
+                // if (action.onClick) {
                 return (
-                  <div key={index}>
-                    <DropdownMenu.Item
-                      className={clx("[&_svg]:text-ui-fg-subtle flex items-center gap-x-2", {
-                        "[&_svg]:text-ui-fg-disabled": action.disabled,
-                      })}
-                      asChild
-                      disabled={action.disabled}
-                    >
-                      <Link to={action.to} onClick={(e) => e.stopPropagation()}>
-                        {action.icon}
-                        <span>{action.label}</span>
-                      </Link>
-                    </DropdownMenu.Item>
-                  </div>
+                  <DropdownMenu.Item
+                    disabled={action.disabled}
+                    key={index}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      action.onClick();
+                    }}
+                    className={clx("[&_svg]:text-ui-fg-subtle flex items-center gap-x-2", {
+                      "[&_svg]:text-ui-fg-disabled": action.disabled,
+                    })}
+                  >
+                    {action.icon}
+                    <span>{action.label}</span>
+                  </DropdownMenu.Item>
                 );
+                // }
+
+                // return (
+                //   <div key={index}>
+                //     <DropdownMenu.Item
+                //       className={clx("[&_svg]:text-ui-fg-subtle flex items-center gap-x-2", {
+                //         "[&_svg]:text-ui-fg-disabled": action.disabled,
+                //       })}
+                //       asChild
+                //       disabled={action.disabled}
+                //     >
+                //       <Link to={action.to} onClick={(e) => e.stopPropagation()}>
+                //         {action.icon}
+                //         <span>{action.label}</span>
+                //       </Link>
+                //     </DropdownMenu.Item>
+                //   </div>
+                // );
               })}
               {!isLast && <DropdownMenu.Separator />}
             </DropdownMenu.Group>
