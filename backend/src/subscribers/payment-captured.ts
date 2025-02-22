0e20d28af027744c004d37a4ca89a51fa445fe0f
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-02-22 08:57:23 +0200

    Small change

--- backend/src/subscribers/payment-captured.ts
+++ backend/src/subscribers/payment-captured.ts
@@ -1,7 +1,20 @@
+import { completeCartWorkflow } from "@medusajs/core-flows";
 import { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
 
 export default async function paymentCapturedHandler({ event: { data }, container }: SubscriberArgs<any>) {
   const logger = container.resolve("logger");
+
+  // 1. Get cart
+
+  // 2. Complete cart
+  //   const { result } = await completeCartWorkflow(container).run({
+  //     input: {
+  //       id: "cart_123",
+  //     },
+  //   });
+
+  //   console.log(result);
+
   logger.info(`⚡🟢 Payment captured: ${JSON.stringify(data)}`);
 }
 
