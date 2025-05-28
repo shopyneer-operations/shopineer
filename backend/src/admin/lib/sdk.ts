d0c3b9c2a7ad8946bdb977183378fa3ce64e8792
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-05-28 15:05:27 +0300

    Small fix

--- backend/src/admin/lib/sdk.ts
+++ backend/src/admin/lib/sdk.ts
@@ -4,7 +4,7 @@ export const sdk = new Medusa({
   baseUrl:
     process.env.NODE_ENV === "development"
       ? "http://localhost:9000"
-      : "https://backend-production-b092.up.railway.app/",
+      : "https://backend-production-f59a.up.railway.app/",
   debug: process.env.NODE_ENV === "development",
   auth: {
     type: "session",
