4c48190bd8a877a520f5cff1c0ccee081c8abac4
Moaaz Bhnas <moaaz_bs@yahoo.com>
2025-08-03 13:10:40 +0300

    Small fix

--- backend/src/api/store/customers/me/returned-orders/route.ts
+++ backend/src/api/store/customers/me/returned-orders/route.ts
@@ -4,12 +4,18 @@ import { ContainerRegistrationKeys } from "@medusajs/framework/utils";
 export const GET = async (req: AuthenticatedMedusaRequest, res: MedusaResponse) => {
   const query = req.scope.resolve(ContainerRegistrationKeys.QUERY);
 
+  const {
+    data: [customer],
+  } = await query.graph({
+    entity: "customer",
+    fields: ["orders.id"],
+    filters: { id: [req.auth_context.actor_id] },
+  });
+
   const data = await query.graph({
     entity: "return",
     fields: ["items.*", "items.reason.*"],
-    filters: {
-      id: [req.auth_context.actor_id],
-    },
+    filters: { order_id: customer.orders.map((order) => order.id) },
   });
 
   res.json({
